9a1053a56ed613c7f76c2622ada63943ea012996
Amitai Efrati <amitai.efrati@gmail.com>
2019-01-06 14:45:48 +0200

    Add reverse lookup for the kin version enum(get enum by value)

--- migration/src/main/java/kin/sdk/migration/interfaces/IKinVersionProvider.java
+++ migration/src/main/java/kin/sdk/migration/interfaces/IKinVersionProvider.java
@@ -1,5 +1,8 @@
 package kin.sdk.migration.interfaces;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import kin.sdk.migration.exception.FailedToResolveSdkVersionException;
 
 public interface IKinVersionProvider {
@@ -9,6 +12,23 @@ public interface IKinVersionProvider {
         NEW_KIN_SDK("3");
 
         private final String version;
+
+        // Lookup table
+        private static final Map<String, SdkVersion> lookup = new HashMap<>();
+
+        // Populate the lookup table on loading time
+        static
+        {
+            for(SdkVersion sdkVersion : SdkVersion.values())
+            {
+                lookup.put(sdkVersion.getVersion(), sdkVersion);
+            }
+        }
+
+        // This method can be used for reverse lookup purpose
+        public static SdkVersion get(String url) {
+            return lookup.get(url);
+        }
 
         SdkVersion(String version) {
             this.version = version;
